2301515822cf596a3b5af6686951a5f3f8ff6fdf
Juan M. Bello Rivas <jmbr@superadditive.com>
2007-11-10 12:41:21 +0100

    Initial revision

---
+++ assemble.lisp
@@ -0,0 +1,46 @@
+(in-package :com.superadditive.incf-cl)
+
+;;; Copyright (c) 2007 Juan M. Bello Rivas <jmbr@superadditive.com>
+;;; 
+;;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;;; of this software and associated documentation files (the "Software"), to deal
+;;; in the Software without restriction, including without limitation the rights
+;;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;;; copies of the Software, and to permit persons to whom the Software is
+;;; furnished to do so, subject to the following conditions:
+;;; 
+;;; The above copyright notice and this permission notice shall be included in
+;;; all copies or substantial portions of the Software.
+;;; 
+;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+;;; THE SOFTWARE.
+
+;;; This code has been adapted from the paper Simple and Efficient
+;;; Compilation of List Comprehension in Common Lisp by Mario
+;;; Latendresse.
+
+(defmacro assemble (collection-form &rest quantifiers)
+  "Assembles a multiset containing the results of evaluating
+COLLECTION-FORM and subject to QUANTIFIERS."
+  (labels ((translate (collection-form qs)
+             (if (null qs)
+                 `(collect ,collection-form)
+                 (translate-generator-or-filter collection-form qs)))
+           (translate-generator-or-filter (collection-form qs)
+             (let ((q (first qs))
+                   (qr (rest qs)))
+               (if (eq (first q) '<-)
+                   (translate-generator collection-form (second q) (third q) qr)
+                   (translate-filter collection-form q qr))))
+           (translate-generator (collection-form variable collection qs)
+             `(nconc (loop for ,variable in ,collection
+                          ,@(translate collection-form qs))))
+           (translate-filter (collection-form filter-form qs)
+             `(when ,filter-form ,@(translate collection-form qs))))
+    (unless (null quantifiers)
+      `(loop repeat 1 ,@(translate collection-form quantifiers)))))
---
+++ incf-cl.asd
@@ -0,0 +1,36 @@
+
+;;; Copyright (c) 2007 Juan M. Bello Rivas <jmbr@superadditive.com>
+;;; 
+;;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;;; of this software and associated documentation files (the "Software"), to deal
+;;; in the Software without restriction, including without limitation the rights
+;;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;;; copies of the Software, and to permit persons to whom the Software is
+;;; furnished to do so, subject to the following conditions:
+;;; 
+;;; The above copyright notice and this permission notice shall be included in
+;;; all copies or substantial portions of the Software.
+;;; 
+;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+;;; THE SOFTWARE.
+
+(defpackage :com.superadditive.incf-cl-system
+  (:use :common-lisp :asdf))
+
+(in-package :com.superadditive.incf-cl-system)
+
+(defsystem "incf-cl"
+  :description "incf-cl is a library of convenience functions for Common Lisp"
+  :version "0.0.1"
+  :author "Juan M. Bello Rivas <jmbr@superadditive.com>"
+  :license "GNU GPLv2"
+  :components ((:file "package")
+               (:file "curry" :depends-on ("package"))
+               (:file "vector" :depends-on ("package"))
+               (:file "assemble" :depends-on ("package"))
+               (:file "range" :depends-on ("package"))))
