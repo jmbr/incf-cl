e370c1a7bd249963bfa081133087659ed789bb4e
Juan M. Bello Rivas <jmbr@superadditive.com>
2007-11-12 03:09:23 +0100

    Forgot to add range.lisp to the repository.

---
+++ range.lisp
@@ -0,0 +1,34 @@
+(in-package :com.superadditive.incf-cl)
+
+;;; Copyright (c) 2007 Juan M. Bello Rivas <jmbr@superadditive.com>
+;;; 
+;;; Permission is hereby granted, free of charge, to any person
+;;; obtaining a copy of this software and associated documentation
+;;; files (the "Software"), to deal in the Software without
+;;; restriction, including without limitation the rights to use, copy,
+;;; modify, merge, publish, distribute, sublicense, and/or sell copies
+;;; of the Software, and to permit persons to whom the Software is
+;;; furnished to do so, subject to the following conditions:
+;;; 
+;;; The above copyright notice and this permission notice shall be
+;;; included in all copies or substantial portions of the Software.
+;;; 
+;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+;;; HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+;;; WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+;;; DEALINGS IN THE SOFTWARE.
+
+;;; Miscellaneous functions to build ranges of numbers (vaguely
+;;; resembling Matlab vector notation).
+
+(defun range (a b &optional c)
+  (flet ((|:| (start step stop)
+           (assert (and (<= start stop) (> step 0)))
+           (loop for x from start to stop by step collect x)))
+    (if c
+        (|:| a b c)
+        (|:| a 1 b))))
