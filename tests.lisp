7aa8a1c51c78c0da7ae456ca7f9ca67c9c9bec68
Juan M. Bello Rivas <jmbr@superadditive.com>
2007-11-19 19:24:16 +0100

    Added an extra test on filter.

--- tests.lisp
+++ tests.lisp
@@ -90,6 +90,8 @@
   (is (equal (multiple-value-list (filter (constantly t) (list 1 2 3)))
              (list (list 1 2 3) nil)))
   (is (equal (multiple-value-list (filter (constantly nil) (list 1 2 3)))
-             (list nil (list 1 2 3)))))
+             (list nil (list 1 2 3))))
+  (is (equal (multiple-value-list (filter #'oddp (list 1 2 3 4 5)))
+             (list (list 1 3 5) (list 2 4)))))
 
 ;;; (incf-cl-test-suite)
