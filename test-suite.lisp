7b3965a46a7d8e6d60f697c606a9ece575467db5
Juan M. Bello Rivas <jmbr@superadditive.com>
2009-04-05 10:36:37 +0200

    Updated test suite to Stefil's current semantics.

--- test-suite.lisp
+++ test-suite.lisp
@@ -1,6 +1,6 @@
 
 ;;; Copyright (c) 2007 Juan M. Bello Rivas <jmbr@superadditive.com>
-;;; 
+;;;
 ;;; Permission is hereby granted, free of charge, to any person
 ;;; obtaining a copy of this software and associated documentation
 ;;; files (the "Software"), to deal in the Software without
@@ -8,10 +8,10 @@
 ;;; modify, merge, publish, distribute, sublicense, and/or sell copies
 ;;; of the Software, and to permit persons to whom the Software is
 ;;; furnished to do so, subject to the following conditions:
-;;; 
+;;;
 ;;; The above copyright notice and this permission notice shall be
 ;;; included in all copies or substantial portions of the Software.
-;;; 
+;;;
 ;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 ;;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 ;;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -22,7 +22,7 @@
 ;;; DEALINGS IN THE SOFTWARE.
 
 ;;; To run test coverage use:
-;;; 
+;;;
 ;;; (progn
 ;;;   (require :sb-cover)
 ;;;   (declaim (optimize sb-cover:store-coverage-data))
@@ -40,7 +40,9 @@
 
 (in-root-suite)
 
-(in-suite (defsuite test))
+(defsuite (test :documentation "incf-cl test suite"))
+
+(in-suite test)
 
 (deftest test-range ()
   (signals error (range 1 -1))
